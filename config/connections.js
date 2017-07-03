3173f67a017fb0c121c8060e5e162a9730d0b0ad
Adam Kwan <adamk1230@gmail.com>
2017-07-02 21:33:19 -0400

    pre heroku deployment

--- config/connections.js
+++ config/connections.js
@@ -1,14 +1,21 @@
 //boilerplate code
 
 var mysql = require("mysql");
+var connection;
 
-var connection = mysql.createConnection({
-  port: 3306,
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "burgers_db"
-});
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+}
+
+else {
+  var connection = mysql.createConnection({
+    port: 3306,
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "burgers_db"
+  });
+}
 
 connection.connect(function(err) {
   if (err) {
